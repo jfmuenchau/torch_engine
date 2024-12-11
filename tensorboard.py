1659344745e7c4b88d5b024f011cc4303dbfc938
jfmuenchau <floh.drum@gmail.com>
2024-12-11 19:54:51 +0100

    added docstrings

--- tensorboard.py
+++ tensorboard.py
@@ -3,6 +3,9 @@ from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
 def clear_tensorboard(path=r"C:\Users\JFM\AppData\Local\Temp\.tensorboard-info"):
+    """Deletes all files in the path folder so that tensorboard can be run in Jupyter Notebooks. Once you start tensorboard a new file will be created in the given path.
+    Trying to load tensorboard again after that file was created will result in an error."""
+    
     for filename in os.listdir(path):
         if os.path.isfile(os.path.join(path, filename)):
             print("Deleting file:", filename)
@@ -10,6 +13,10 @@ def clear_tensorboard(path=r"C:\Users\JFM\AppData\Local\Temp\.tensorboard-info")
 
 
 def create_summarywriter(model_name:str, extra:str=None):
+    """Creates a SummaryWrite from torch.utils.tensorboard.SummaryWriter that can be used to track parameters during training.
+    * model_name: Name of the model thats going to be trained.
+    * extra: Extra string to append to the model name. Will create a new folder in "runs/date/model_name/extra". The SummaryWriter will save to this folder.
+    """
     date=datetime.now().strftime("%Y-%m-%d")
 
     if extra:
